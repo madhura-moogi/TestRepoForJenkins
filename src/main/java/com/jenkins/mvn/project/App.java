e3265f2b88ba12219b6830204895bb448765f863
Madhura_Moogi <madhura.moogi@gmail.com>
2024-04-18 19:42:38 +0530

    initial commit

---
+++ src/main/java/com/jenkins/mvn/project/App.java
@@ -0,0 +1,13 @@
+package com.jenkins.mvn.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
